843636fa55bf34fde6f6396df686bc6e50a6395f
Ruel Nopal <radon1284@yahoo.com>
2023-06-24 17:18:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,21 @@ echo "file_uploads = On" >> ./php/uploads.ini
 echo "upload_max_filesize = 64M" >> ./php/uploads.ini
 echo "post_max_size = 64M" >> ./php/uploads.ini
 
-# Run docker-compose
+# Check if Docker Compose is installed
+if ! command -v docker-compose &> /dev/null
+then
+    echo "Docker Compose could not be found, installing..."
+    # Install Docker Compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+else
+    echo "Docker Compose is already installed, skipping installation."
+fi
+
+# Verify Docker Compose installation
+docker-compose --version
+
+# Run Docker Compose
 docker-compose up -d
 
 # chmod +x run.sh
